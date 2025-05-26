48b11cd1e8ea15cb02d142795c9ee8851c336e8a
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-05-26 13:18:14 +0530

    updated palindrom

--- palindrom.py
+++ palindrom.py
@@ -1,8 +1,23 @@
 #By using slicing
 
-n = input("Enter any string:- ")
+# n = input("Enter any string:- ")
 
-if (n==n[::-1]):
+# if (n==n[::-1]):
+#     print(f"{n} is palindrom")
+# else: 
+#     print(f"{n} is not palindrom")
+
+
+
+
+# By using for loop
+
+n= input("Enter the value:- ")
+rev = ''
+for i in n:
+    rev = rev+i
+
+if n == rev: 
     print(f"{n} is palindrom")
-else: 
+else:
     print(f"{n} is not palindrom")
