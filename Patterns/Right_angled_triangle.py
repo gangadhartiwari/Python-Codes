ccfad6ea7015e1cc44aa7f09878b488fe5fd986d
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-05-27 13:31:59 +0530

    right angled triangle pattern

---
+++ Patterns/Right_angled_triangle.py
@@ -0,0 +1,19 @@
+# by using for loop
+
+# n = int(input("Enter the row number:- "))
+
+# for i in range(1, n+1): 
+#     print('* '*i)             # there is no need to print ' '(space) 
+                                # because space needed after the star then no need to print it.
+
+
+
+
+# by using while loop 
+
+n= int(input("Enter row number:- "))
+
+i = 1
+while i<=n: 
+    print('* '*i)
+    i = i+1
