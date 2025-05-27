4d7e9519b10ee902930acef88a067b37013030bc
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-05-27 13:36:55 +0530

    inverted right angled traingle

---
+++ Patterns/inverted_right_angled_triangle.py
@@ -0,0 +1,18 @@
+# by using for loop
+
+# n = int(input("Enter the row number:- "))
+
+# for i in range(n, 0, -1): 
+#     print('* '*i) 
+
+
+
+
+
+# by using while loop
+
+n = int(input("Enter the row number:- "))
+
+while n>0: 
+     print('* '*n)
+     n= n-1
