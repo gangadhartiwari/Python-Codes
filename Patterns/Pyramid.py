666bacb0e2d509dd230c22a8f54b7eb03f6d9cfd
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-05-28 15:36:28 +0530

    updated pyramid

--- Patterns/Pyramid.py
+++ Patterns/Pyramid.py
@@ -1,6 +1,19 @@
 # By using for loop
 
+# n= int(input("Enter the row number:- "))
+
+# for i in range(1, n+1): 
+#     print(' '*(n-i)+ ' *'*i)
+
+
+
+
+
+# by using while loop 
+
 n= int(input("Enter the row number:- "))
 
-for i in range(1, n+1): 
+i = 1
+while i<=n: 
     print(' '*(n-i)+ ' *'*i)
+    i += 1
