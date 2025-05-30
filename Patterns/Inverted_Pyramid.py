873b7aa47051e5abc269d5bd25e24124c809113c
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-05-31 00:52:44 +0530

    okk

--- Patterns/Inverted_Pyramid.py
+++ Patterns/Inverted_Pyramid.py
@@ -1,8 +1,15 @@
 # using for loop 
 
-rows = 5
-for i in range(rows, 0, -1):
-    print(" " * (rows - i) + "*" * (2 * i - 1))
+# rows = 5
+# for i in range(rows, 0, -1):
+#     print(" " * (rows - i) + "*" * (2 * i - 1))
 
 
 # using while loop
+
+n= int(input("Enter the row number:- "))
+
+i = 0
+while i<=n: 
+    print(' '*i+ ' *'*(n-i))
+    i += 1
