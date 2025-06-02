b31d52956aa26b9f28af51444d48a5ef6e4e42e4
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-06-02 23:56:09 +0530

    basic function code

---
+++ Functions/basic.py
@@ -0,0 +1,12 @@
+# In Python, a function is a block of organized, reusable code that performs a specific task. 
+# Functions help break down code into smaller, manageable parts, making it more readable and maintainable.
+
+
+def add_numbers(num1, num2):
+    """Adds two numbers and returns the sum."""
+    sum = num1 + num2
+    return sum
+
+# Function call
+result = add_numbers(5, 3)
+print(result)  # Output: 8
