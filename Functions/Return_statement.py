2900ae668148e90fab3064e71680f65a29b7ce6d
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-06-05 15:13:15 +0530

    return statement

---
+++ Functions/Return_statement.py
@@ -0,0 +1,11 @@
+# We return a value from the function using the return statement.
+
+
+# function definition
+def find_square(num):
+    result = num * num
+    return result
+
+# function call
+square = find_square(3)
+print('Square:', square)
