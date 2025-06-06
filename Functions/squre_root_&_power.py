2d47f18c79eecc655e0800572577fc39a5e6261e
gangadhartiwari <tiwarigangadhar840@gmail.com>
2025-06-06 23:31:02 +0530

    sqrt

---
+++ Functions/squre_root_&_power.py
@@ -0,0 +1,18 @@
+import math
+
+n = int(input("Enter The Number :- "))
+
+# sqrt computes the square root
+square_root = math.sqrt(n)
+
+print(f"Square Root of {n}  is : ",square_root)
+
+
+
+# --------------------------------------------------------------------------
+
+
+# pow() comptes the power
+power = pow(2, 3)
+
+print("2 to the power 3 is : ",power)
